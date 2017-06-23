75a65303c6d5d1bcb36b7c2c68218e1253c54e69
Bnjamin Misell <beninjam174@gmail.com>
2017-06-23 08:03:44 +0100

    Move to global world instance initalized on import and saved in module

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import pyscratch
 import time
 
-world = pyscratch.init_world()
-
-cat = pyscratch.get_scratchcat(world)
+cat = pyscratch.get_scratchcat()
 
 
-@pyscratch.on_start(world)
+@pyscratch.on_start
 def main():
     cat.set_scale_to(125)
     while True:
         cat.point_to_mouse()
         time.sleep(0.1)
 
-world.run()
+pyscratch.run()
--- pyscratch/__init__.py
+++ pyscratch/__init__.py
@@ -1,25 +1,24 @@
 from . import pyScratch
 from . import sprite
 
-
-def init_world():
-    return pyScratch.World((500, 400), 60)
+world = pyScratch.World((500, 400), 60)
 
 
-def get_scratchcat(world):
+def get_scratchcat():
     s = sprite.Sprite('scratchcat.png', world)
     world.inject_sprite(s)
     return s
 
 
-def get_sprite(world, img):
+def get_sprite(img):
     s = sprite.Sprite(img, world)
     world.inject_sprite(s)
     return s
 
 
-def on_start(world):
-    def bind(func):
-        world.bind_on_start(func)
-        return func
-    return bind
+def on_start(func):
+    world.bind_on_start(func)
+    return func
+
+def run():
+    world.run()
