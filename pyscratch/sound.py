25e876a85e8d15d0ad2442842699936c78ebd8e5
Benjamin Misell <beninjam174@gmail.com>
2017-06-23 13:21:09 +0100

    Global sound

---
+++ pyscratch/sound.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class Sound:
+    def __init__(self, world):
+        self._world = world
+
+    #########################
+    # Scratch sound section #
+    #########################
+
+    def play_sound(self, sound):
+        self._world.resources.play_sound(sound)
+
+    def play_sound_until_done(self, sound):
+        self._world.resources.play_sound(sound)
+        if pygame.mixer.get_busy():
+            pass
+
+    def stop_all_sounds(self):
+        pygame.mixer.stop()
+
+    def set_volume(self, volume):
+        self._world.resources.set_volume(volume/100)
+
+    def change_volume_by(self, volume):
+        old_vol = self.volume
+        new_vol = old_vol + volume/100
+        self._world.resources.set_volume(new_vol)
+
+    @property
+    def volume(self):
+        return self._world.resources.volume
